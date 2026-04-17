f7eeffbdd9f4bd708b4811cfa84e4fc1d590170f
Jenny Zhang <jennyzhang0207@gmail.com>
2026-04-17 13:57:17 +0100

    Contact page mobile and desktop styling

--- theme/theprintbank/contact-us.php
+++ theme/theprintbank/contact-us.php
@@ -1,15 +1,15 @@
 <?php /* Template Name: Contact Page */ ?>
 <?php get_header(); ?>
 
-<nav class="d-flex flex-row justify-content-between pt-3 mx-2">
+<nav class="d-flex flex-row justify-content-between pt-3 mx-3">
   <a class="nav-links contact <?php if(is_front_page()) { echo 'active';} ?>" href="<?php echo home_url(); ?>">Home</a>
   <a class="nav-links contact <?php if(is_page(10)) { echo 'active';} ?>" href="<?php  echo get_page_link(10); ?>">Contact Us</a>
 </nav>
 
-<div class="container text-left d-flex- flexnowrap">
-  <div class="row mb-md-3">
-    <div class="col-md-6 col-12 order-1">    
-      <article class="mx-2 mt-5 pt-md-5"> 
+<div class="container text-left">
+  <div class="row mb-md-3 mt-5">
+    <div class="col-md-6 order-1">    
+      <article class="mx-2 pt-md-5"> 
         <p class="fw-normal fs-2">Contact Us</p>
         <div class="py-3">
           <span class="fw-light">Email</span>
@@ -37,20 +37,17 @@
     </div>
 
     <div class="col-md-6 order-3 order-md-2"> 
-      <div class="mx-2 mt-md-5 pt-md-5">
+      <div class="mx-2 pt-md-5">
         <p class="fw-normal py-md-0 py-3 fs-2">Request a quote</p>
-        <div class="wpcf7-form-container">
           <?php echo do_shortcode('[contact-form-7 id="69d5731" title="Contact form 1"]'); ?>
-        </div>
       </div>
     </div>
-    
-    <div class="col-12 order-2 order-md-3">
+
+    <div class="order-2 order-md-3 mt-md-5">
       <div class="mx-2 mb-4">
         <?php echo do_shortcode('[wpgmza id="1"]'); ?>
       </div>
     </div>
   </div>
-
 </div>
 <?php get_footer(); ?>
